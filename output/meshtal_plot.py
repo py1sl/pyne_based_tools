364abcf3b66ded7ee9d56d3f25c7794938d35c4e
py1sl <steven.lilley@stfc.ac.uk>
2016-04-06 03:47:45 -0700

    meshtal convert simplified

--- output/meshtal_plot.py
+++ output/meshtal_plot.py
@@ -2,20 +2,16 @@ from pyne import mcnp
 from itaps import iBase, iMesh
 import h5py
 import sys
-from subprocess import call
 
 
 def convert_meshtal(argv):
     """simple convert meshtal to vtk via h5m """
-    in_path=argv[0]
-    out_path=argv[1]
+    in_path = argv[0]
+    out_path = argv[1]
     
-    meshtal1=mcnp.Meshtal(in_path)
+    meshtal1 = mcnp.Meshtal(in_path)
     meshtal1.tally[14].mesh.save(out_path + ".vtk")
 
-    call(["expand_tags.py", out_path+".h5m" , "-o", out_path+".vtk"])
-    call(["rm", out_path+".h5m"])
-
-
-if __name__=="__main__":
+   
+if __name__ == "__main__":
     convert_meshtal(sys.argv[1:])
