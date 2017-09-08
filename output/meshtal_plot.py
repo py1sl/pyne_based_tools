41ef72864eea1885e9ef1b83fbc603e6d09dda6f
Steven Lilley <steven.lilley@stfc.ac.uk>
2017-09-08 08:38:37 -0700

    updated user options in meshtal

--- output/meshtal_plot.py
+++ output/meshtal_plot.py
@@ -3,15 +3,14 @@ from itaps import iBase, iMesh
 import h5py
 import sys
 from subprocess import call
+import argparse
 
 
-def convert_meshtal(argv):
+def convert_meshtal(in_path, tnum, out_path="meshtal"):
     """simple convert meshtal to vtk via h5m """
-    in_path = argv[0]
-    out_path = argv[1]
-    tnum = int(argv[2])
-    
+        
     meshtal1 = mcnp.Meshtal(in_path)
+    print(meshtal1.tally)
     meshtal1.tally[tnum].mesh.save(out_path + ".vtk")
 
 
@@ -20,4 +19,14 @@ def convert_meshtal(argv):
 
    
 if __name__ == "__main__":
-    convert_meshtal(sys.argv[1:])
+    parser = argparse.ArgumentParser(description="Convert MCNP mesh tally to vtk format")
+    parser.add_argument("input", help="path to the input file")
+    parser.add_argument("tally", help="tally number to convert", default=1, type=int )
+    parser.add_argument("-o", "--output", action="store", dest="output",  help="path to the output file without filetype")
+
+    args = parser.parse_args()    
+
+    if args.output:
+        convert_meshtal(args.input, args.tally, args.output)
+    else:
+        convert_meshtal(args.input, args.tally)
