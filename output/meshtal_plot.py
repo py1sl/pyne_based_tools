92bd8865b9e9de78d24a2e635e0f2e826bad5e61
Steven Lilley <steven.lilley@stfc.ac.uk>
2017-09-12 02:49:35 -0700

    add tag option

--- output/meshtal_plot.py
+++ output/meshtal_plot.py
@@ -6,27 +6,41 @@ from subprocess import call
 import argparse
 
 
-def convert_meshtal(in_path, tnum, out_path="meshtal"):
+def convert_meshtal(in_path, tnum, out_path="meshtal", tags=False):
     """simple convert meshtal to vtk via h5m """
         
     meshtal1 = mcnp.Meshtal(in_path)
     print(meshtal1.tally)
-    meshtal1.tally[tnum].mesh.save(out_path + ".vtk")
+    print(meshtal1.tally[tnum].mesh.tag)
+    if tags:
+        meshtal1.tally[tnum].mesh.save(out_path + ".h5m")
+        call(["expand_tags.py", out_path+".h5m" , "-o", out_path+".vtk"])
+        call(["rm", out_path+".h5m"])
+
+    else:
+        meshtal1.tally[tnum].mesh.save(out_path + ".vtk")
 
 
-    #call(["expand_tags.py", out_path+".h5m" , "-o", out_path+".vtk"])
-    #call(["rm", out_path+".h5m"])
 
    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert MCNP mesh tally to vtk format")
     parser.add_argument("input", help="path to the input file")
     parser.add_argument("tally", help="tally number to convert", default=1, type=int )
-    parser.add_argument("-o", "--output", action="store", dest="output",  help="path to the output file without filetype")
+    parser.add_argument("-o", "--output", action="store", dest="output",  
+                        help="path to the output file without filetype")
+    parser.add_argument("-t", "--tags", action="store_true", dest="tags",  
+                        help="use this if want all tags to be expanded and results for all")
 
     args = parser.parse_args()    
 
     if args.output:
-        convert_meshtal(args.input, args.tally, args.output)
+        if args.tags:
+            convert_meshtal(args.input, args.tally, args.output, True)
+        else:
+            convert_meshtal(args.input, args.tally, args.output)
     else:
-        convert_meshtal(args.input, args.tally)
+        if args.tags:
+            convert_meshtal(args.input, args.tally, "meshtal", True)
+        else:
+            convert_meshtal(args.input, args.tally)
